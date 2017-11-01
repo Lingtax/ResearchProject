6c0f032797743e4ca17d3a4090fd0a870bc08def
Mathew Ling <m.ling@deakin.edu.au>
2017-11-01 17:36:57 +1100

    Revises scaffold

---
+++ R/scaffold.R
@@ -0,0 +1,19 @@
+scaffold <- function() {
+  print("Creating folder structure...")
+  if (!dir.exists("./data/"))     dir.create("./data/")
+  if (!dir.exists("./helpers/"))  dir.create("./helpers/")
+  if (!dir.exists("./scripts/")) dir.create("./scripts/")
+  if (!dir.exists("./output/")) dir.create("./output/")
+
+  print("Initialising git...")
+  git2r::init(".")
+  x <- ".Rproj.user
+.Rhistory
+.RData
+.Ruserdata
+data/"
+  write(x, ".gitignore")
+  print("Linking to github...")
+  usethis::use_github()
+ }
+
